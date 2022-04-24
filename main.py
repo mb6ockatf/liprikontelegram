d1763a933723dcb53593f0ba08ae0fec59f90ef4
BigProject404 <bigproject404@yandex.ru>
2022-04-24 12:31:23 +0300

    Update main.py
    Now the repo autofetches itself, so consider turning this feature off
    when testing!
    Also, skeleton of some new features added. Dependency bugs fixed.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from threading import Thread
 import telebot
 from time import sleep
-from datetime import time
+from datetime import datetime as dt
 from os import environ, system
 
 token = str(environ.get('token'))
@@ -20,7 +20,7 @@ def ping(message):
 
 @bot.message_handler(commands=['tm'])
 def tm(message):
-    answer(message, f"{good}{time.time()}")
+    answer(message, f"{good}{dt.now()}")
 
 
 @bot.message_handler(content_types=['new_chat_members'])
@@ -35,22 +35,46 @@ def timer(message):
     except IndexError:
         answer(message, f"{bad}No time value provided")
         return
-    start = datetime.datetime.now()
+    start = dt.now()
     answer(message, f"{good}Count {delta} secs since {start}")
-    time.sleep(delta)
+    sleep(delta)
     send(message.chat.id, f"{good}Your timer is ringing\n{delta} secs have passed since {start}")
 
 
-@bot.message_handler(commands=['time'])
-def show_time(message):
-    send(message.chat.id, f"{good}{datetime.datetime.now()}")
+@bot.message_handler(commands=['search'])
+def search(message):
+    link_contents = " ".join(message.text.split()[1:])
+    g = f'https://yandex.ru/?q={link_contents}' + '\n\n' + \
+        f'https://www.google.com/search?q={link_contents}'
+    bot.send_message(message.chat.id, g, disable_web_page_preview=True)
+    # Send yourself: bot.send_message(message.from_user.id, f"https://yandex.ru/?q={link_contents}")
 
 
-def autofetch():
+@bot.message_handler(commands=['anywhere', 'fsearch', 'full'])
+def full_search(message):
+    link_contents = " ".join(message.text.split()[1:])
+    engines = ['https://yandex.ru/?q=',
+               'https://www.google.com/search?q=',
+               'https://duckduckgo.com/?q=',
+               'https://m.wikipedia.org/w/index.php?search=',
+               'https://m.youtube.com/results?search_query=',
+               'https://discord.com/guild-discovery?query=']
+    g = "\n\n".join(list(map(lambda x: x + link_contents, engines)))
+    bot.send_message(message.chat.id, g, disable_web_page_preview=True)
+
+
+"""@bot.message_handler(commands=['translate'])
+def full_search(message):
+    link_contents = " ".join(message.text.split()[1:])
+    # TODO"""
+
+
+def auto_fetch():
     while True:
         system('git fetch')
         sleep(3600)
 
+
 if __name__ == '__main__':
     Thread(target=bot.infinity_polling).start()
-    Thread(target=autofetch).start()
+    Thread(target=auto_fetch).start()
